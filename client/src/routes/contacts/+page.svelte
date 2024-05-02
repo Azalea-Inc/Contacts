87bc691f64d88815a607965da6f5f1ec35d62812
Gilberto28Xx <gilnupradu@hotmail.com>
2024-05-01 23:23:51 -0500

    feat: Lista de contactos modificada (implementacion de CSS con tailwind)

--- client/src/routes/contacts/+page.svelte
+++ client/src/routes/contacts/+page.svelte
@@ -1,22 +1,44 @@
 <script>
-  import Counter from "../../views/Counter.svelte";
+  import Table from "../../views/Table.svelte";
 
-  let contadores = [
-    { title: "Titulo1" },
-    { title: "Titulo2" },
-    { title: "Titulo3" },
-    { title: "Titulo4" },
+  let titles = [
+    'Nombre',
+    'Apellido',
+    'Correo electronico',
+    'Numero',
+    'Acciones'
+  ];
+
+  let contacts = [
+    {
+      name: "Joan",
+      lastname: "Coronado",
+      mail: "joancoronado@gmail.com",
+      phone: "8687893945",
+    },
+    {
+      name: "Fidel",
+      lastname: "Araguz",
+      mail: "fidelaraguz@gmail.com",
+      phone: "8687895069",
+    },
+    {
+      name: "Gilberto",
+      lastname: "Garza",
+      mail: "gilbertogarza@gmail.com",
+      phone: "8687895004",
+    },
   ];
 </script>
 
-<h1>Contacts</h1>
+<!-- Titles with buttons -->
+<div class="w-9/12 mx-auto">
+  <div class="flex justify-between my-5">
+    <h3 class="text-3xl font-semibold">Contactos</h3>
+    <a href="/contacts/new" class="p-2  border-2 rounded-md border-sky-500 text-white bg-sky-900">
+      Agregar contacto
+    </a>
+  </div>
+</div>
 
-{#each contadores as contador}
-  <Counter title={contador.title}></Counter>
-{/each}
-
-<style>
-  h1 {
-    color: #ff0000;
-  }
-</style>
+<Table {contacts} {titles}></Table>
---
+++ client/src/views/Table.svelte
@@ -0,0 +1,81 @@
+<script>
+  export let contacts = [];
+  export let titles = [];
+</script>
+
+<div class="w-9/12 mx-auto">
+  <!-- Contact list -->
+  <table class="w-full">
+    <!-- Header from our table -->
+    <thead>
+      <tr class="text-center">
+        {#each titles as title}
+          <th class="p-4"> {title} </th>
+        {/each}
+      </tr>
+    </thead>
+    <!-- This is the table body -->
+    <tbody>
+      {#each contacts as contact}
+        <tr class="text-center border-solid border-b-4">
+          <!-- Name -->
+          <td class="p-4">
+            {contact.name}
+          </td>
+          <!-- Lastname -->
+          <td>
+            {contact.lastname}
+          </td>
+          <!-- Mail -->
+          <td>
+            {contact.mail}
+          </td>
+          <!-- Phone -->
+          <td>
+            {contact.phone}
+          </td>
+          <!-- Actions -->
+          <td>
+            <button class="px-2 border-2 rounded-md bg-cyan-300"> Editar </button>
+            <button class="px-2 border-2 rounded-md bg-red-800 text-white"> Eliminar </button>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+<!--
+
+  <div class="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
+    <table class="w-full table-fixed">
+      <thead>
+        <tr class="bg-gray-100">
+          {#each titles as title}
+            <th
+              class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase"
+              >{title}</th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody class="bg-white">
+        {#each contacts as contact}
+          <tr>
+            <td class="py-4 px-6 border-b border-gray-200">{contact.name}</td>
+            <td class="py-4 px-6 border-b border-gray-200 truncate"
+              >{contact.mail}</td
+            >
+            <td class="py-4 px-6 border-b border-gray-200">{contact.phone}</td>
+            <td class="py-4 px-6 border-b border-gray-200"></td>
+            <td class="py-4 px-6 border-b border-gray-200"></td>
+            <td class="py-4 px-6 border-b border-gray-200">
+              <span class="bg-green-500 text-white py-1 px-2 rounded-full text-xs"
+                >Active</span
+              >
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+-->
